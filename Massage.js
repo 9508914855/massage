c4d8eb06532fc0ea3fe7524cc9964d3bc280b324
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-05-13 20:01:45 +0530

    Update Massage.js

--- Massage.js
+++ Massage.js
@@ -4,6 +4,28 @@ const messageElement = document.getElementById('message');
 const shakeButton = document.getElementById('shake-button');
 const shareButton = document.getElementById('share-button');
 
+// create share function
+const share = async () => {
+    try {
+        const token = generateToken();
+        const shareUrl = `${window.location.origin}${window.location.pathname}?t=${token}`;
+        await navigator.share({
+            title: 'Custom Message Card',
+            text: 'Check out this custom message card',
+            url: shareUrl
+        });
+    } catch (err) {
+        alert('Sharing is not supported on this device.');
+    }
+};
+
+// generate one-time token
+const generateToken = () => {
+    const randomString = Math.random().toString(36).substring(7);
+    const timestamp = Date.now();
+    return `${randomString}-${timestamp}`;
+};
+
 // add click event listener to shake button
 shakeButton.addEventListener('click', () => {
     cardElement.classList.add('shake');
@@ -12,48 +34,28 @@ shakeButton.addEventListener('click', () => {
     }, 1000);
 });
 
-// generate a random token and store it in local storage
-const generateToken = () => {
-  return Math.random().toString(36).substr(2, 9);
-};
-
 // add click event listener to share button
 shareButton.addEventListener('click', () => {
-  // generate a new token and create a share link with the message and token in the query parameters
-  const token = generateToken();
-  const shareUrl = `${window.location.origin}${window.location.pathname}?message=${encodeURIComponent(messageElement.innerText)}&token=${encodeURIComponent(token)}`;
-
-  // show share dialog if supported, otherwise prompt user to copy the link
-  if (navigator.share) {
-    navigator.share({
-      title: 'Custom Message Card',
-      text: messageElement.innerText,
-      url: shareUrl,
-    });
-  } else {
-    prompt('Copy this URL and share it with others:', shareUrl);
-  }
+    if (navigator.share) {
+        share();
+    } else {
+        const token = generateToken();
+        const shareUrl = `${window.location.origin}${window.location.pathname}?t=${token}`;
+        prompt('Copy this URL and share it with others:', shareUrl);
+    }
 });
 
-// check if a message and token are present in the URL parameters
+// check if token is in URL parameters and display message if it is valid
 const urlParams = new URLSearchParams(window.location.search);
-const message = urlParams.get('message');
-const token = urlParams.get('token');
-
-if (message && token) {
-  // if a message and token are present, check if the token is valid
-  const viewedToken = localStorage.getItem(token);
-
-  if (viewedToken === null) {
-    // if the token is valid, show the message and store the token in local storage
-    messageElement.innerText = message;
-    localStorage.setItem(token, true);
-  } else {
-    // if the token has already been viewed, show the default message
+const tokenParam = urlParams.get('t');
+const validToken = localStorage.getItem('validToken');
+if (tokenParam && tokenParam === validToken) {
     messageElement.innerText = 'Enter your message here';
-  }
+    localStorage.removeItem('validToken');
 } else {
-  // if no message or token are present, show the default message
-  messageElement.innerText = 'Enter your message here';
+    messageElement.innerText = 'Enter your message here';
 }
 
+// store new token in localStorage and remove previous one
+const newToken = generateToken();
+localStorage.setItem('validToken', newToken);
