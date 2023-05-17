caee049c546ecf385e7ea8bc96341df2f584a68f
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-05-17 06:41:04 +0530

    Update Massage.js

--- Massage.js
+++ Massage.js
@@ -1,69 +1,28 @@
-// Generate a random token
-const generateToken = () => {
-  return Math.random().toString(36).substr(2, 9);
+// Generate a unique token using MD5 hashing algorithm
+const generateToken = (message) => {
+  const hash = CryptoJS.MD5(message).toString();
+  return hash.substring(0, 8);
 };
-
-// Shorten URL using base64 encoding
-const shortenUrl = (url) => {
-  const encodedUrl = btoa(url);
-  const encodedUrlWithoutPadding = encodedUrl.replace(/=+$/, ''); // Remove trailing padding characters (=)
-  const shortenedUrl = encodedUrlWithoutPadding.substring(0, 8);
-  return shortenedUrl;
-};
-
-
-// Get DOM elements
-const shareButton = document.getElementById('share-button');
-const messageElement = document.getElementById('message');
-const titleElement = document.getElementById('title');
-const infoButton = document.getElementById('info-button');
 
 // Add click event listener to share button
 shareButton.addEventListener('click', () => {
-  // Generate a new token
-  const token = generateToken();
-
-  // Create a share link with the message, title, and token in the query parameters
-  const shareUrl = `${window.location.origin}${window.location.pathname}?message=${encodeURIComponent(messageElement.innerText)}&title=${encodeURIComponent(titleElement.innerText)}&token=${encodeURIComponent(token)}`;
-
-  // Shorten the share URL
-  const shortShareUrl = shortenUrl(shareUrl);
-
-  // Show share dialog if supported, otherwise prompt the user to copy the link
+  const message = messageElement.innerText;
+  const title = titleElement.innerText;
+  
+  // Generate a token based on the message content
+  const token = generateToken(message);
+  
+  // Create the share URL with the message, title, and token as query parameters
+  const shareUrl = `${window.location.origin}${window.location.pathname}?message=${encodeURIComponent(message)}&title=${encodeURIComponent(title)}&token=${encodeURIComponent(token)}`;
+  
+  // Show share dialog if supported, otherwise prompt user to copy the link
   if (navigator.share) {
     navigator.share({
       title: 'Custom Message Card',
-      text: 'Click 👉 ',
-      url: shortShareUrl,
+      text: 'Click 👉',
+      url: shareUrl,
     });
   } else {
-    prompt('Copy this URL and share it with others:', shortShareUrl);
+    prompt('Copy this URL and share it with others:', shareUrl);
   }
 });
-
-// Add click event listener to info button
-infoButton.addEventListener('click', () => {
-  alert('Introducing the latest tool by Shashi: the One-Time Message Sender. Iss tool ki madad se aap kisi ko message bhej sakte ho, jo sirf ek baar dikhayi dega, phir hamesha ke liye delete ho jaayega. Yeh tool sensitive information aur private conversations ke liye accha option hai.');
-});
-
-// Check if a message, title, and token are present in the URL parameters
-const urlParams = new URLSearchParams(window.location.search);
-const message = urlParams.get('message');
-const title = urlParams.get('title');
-const token = urlParams.get('token');
-
-if (message && title && token) {
-  // If a message, title, and token are present, show the message and title
-  messageElement.innerText = message;
-  titleElement.innerText = title;
-
-  // Remove the token from the URL to prevent sharing the same message multiple times
-  const urlWithoutToken = `${window.location.origin}${window.location.pathname}`;
-
-  // Replace the URL in the browser's history without adding an entry
-  window.history.replaceState({}, document.title, urlWithoutToken);
-} else {
-  // If no message, title, or token are present, show the default message and title
-  messageElement.innerText = 'Enter your message here';
-  titleElement.innerText = 'Enter Your Name';
-}
