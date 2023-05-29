6f96ca77dc2a679abaeb7954a96b322757c5005f
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-05-29 20:14:58 +0530

    Update Massage.js

--- Massage.js
+++ Massage.js
@@ -4,43 +4,44 @@ const titleElement = document.getElementById('title');
 const shareButton = document.getElementById('share-button');
 const infoButton = document.getElementById('info-button');
 
-// generate a random token and store it in local storage
-const generateToken = () => {
-  return Math.random().toString(36).substr(2, 9);
-};
-
 // add click event listener to share button
 shareButton.addEventListener('click', async () => {
   // generate a new token and create a share link with the message, title, and token in the query parameters
   const token = generateToken();
   const shareUrl = `${window.location.origin}${window.location.pathname}?message=${encodeURIComponent(messageElement.innerText)}&title=${encodeURIComponent(titleElement.innerText)}&token=${encodeURIComponent(token)}`;
 
-  // shorten the shareUrl using bitelink API
-  const apiEndpoint = 'https://bitelink.000webhostapp.com/api.php';
-  const response = await fetch(apiEndpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ longUrl: shareUrl })
-  });
+  try {
+    // send a POST request to the bitelink API to shorten the shareUrl
+    const response = await fetch('https://bitelink.000webhostapp.com/api.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        longUrl: shareUrl
+      })
+    });
 
-  if (response.ok) {
-    const data = await response.json();
-    const shortUrl = data.shortUrl;
-    
-    // show share dialog if supported, otherwise prompt user to copy the link
-    if (navigator.share) {
-      navigator.share({
-        title: 'Custom Message Card',
-        text: 'Click 👉 ',
-        url: shortUrl,
-      });
+    if (response.ok) {
+      const responseData = await response.json();
+      const shortUrl = responseData.shortUrl;
+
+      // show share dialog if supported, otherwise prompt user to copy the link
+      if (navigator.share) {
+        navigator.share({
+          title: 'Custom Message Card',
+          text: 'Click 👉 ',
+          url: shortUrl,
+        });
+      } else {
+        prompt('Copy this URL and share it with others:', shortUrl);
+      }
     } else {
-      prompt('Copy this URL and share it with others:', shortUrl);
+      throw new Error('Failed to shorten the URL');
     }
-  } else {
-    alert('Error: Failed to shorten the URL');
+  } catch (error) {
+    console.error(error);
+    alert('Error occurred while shortening the URL. Please try again later.');
   }
 });
 
@@ -66,11 +67,16 @@ if (message && title && token) {
     localStorage.setItem(token, true);
   } else {
     // if the token has already been viewed, show the default message and title
-    messageElement.innerText = 'This message has already been viewed. Tap to edit and send.';
+    messageElement.innerText = 'This message has been already viewed. Tap to edit and send.';
     titleElement.innerText = 'Sorry';
   }
 } else {
   // if no message, title, or token are present, show the default message and title
   messageElement.innerText = 'Enter your message here';
   titleElement.innerText = 'Enter Your Name';
+}
+
+// generate a random token and store it in local storage
+function generateToken() {
+  return Math.random().toString(36).substr(2, 9);
 }
