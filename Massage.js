db78a45f4ff33fa8d53e5fd792a27204deea4ce9
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-06-01 10:08:32 +0530

    Update Massage.js

--- Massage.js
+++ Massage.js
@@ -1,76 +1,91 @@
-const cardElement = document.getElementById('card');
-const messageElement = document.getElementById('message');
-const titleElement = document.getElementById('title');
-const shareButton = document.getElementById('share-button');
-const infoButton = document.getElementById('info-button');
+document.addEventListener('DOMContentLoaded', () => {
+  const cardElement = document.getElementById('card');
+  const messageElement = document.getElementById('message');
+  const titleElement = document.getElementById('title');
+  const shareButton = document.getElementById('share-button');
+  const infoButton = document.getElementById('info-button');
 
-// generate a random token and store it in local storage
-const generateToken = () => {
-  return Math.random().toString(36).substr(2, 9);
-};
+  // generate a random token and store it in local storage
+  const generateToken = () => {
+    return Math.random().toString(36).substr(2, 9);
+  };
 
-// add click event listener to share button
-shareButton.addEventListener('click', async () => {
-  // generate a new token and create a share link with the message, title, and token in the query parameters
-  const token = generateToken();
-  const shareUrl = `${window.location.origin}${window.location.pathname}?message=${encodeURIComponent(messageElement.innerText)}&title=${encodeURIComponent(titleElement.innerText)}&token=${encodeURIComponent(token)}`;
+  // add click event listener to share button
+  shareButton.addEventListener('click', async () => {
+    // generate a new token and create a share link with the message, title, and token in the query parameters
+    const token = generateToken();
+    const shareUrl = `${window.location.origin}${window.location.pathname}?message=${encodeURIComponent(
+      messageElement.innerText
+    )}&title=${encodeURIComponent(titleElement.innerText)}&token=${encodeURIComponent(token)}`;
 
-  // Validate the shareUrl
-  if (!shareUrl.trim()) {
-    alert('Invalid share URL');
-    return;
-  }
-
-  // send the shareUrl to your PHP script for shortening
-  const apiEndpoint = 'https://bitelink.000webhostapp.com/api.php'; // Replace with your PHP script URL
-  const response = await fetch(apiEndpoint, {
-    method: 'POST',
-    body: JSON.stringify({ longUrl: shareUrl }), // Pass the shareUrl as 'longUrl'
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    // Make a POST request to the API endpoint to shorten the URL
+    fetch('https://bitelink.000webhostapp.com/api.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ longUrl: shareUrl }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'success') {
+          const shortUrl = data.shortUrl;
+          // show share dialog if supported, otherwise prompt user to copy the link
+          if (navigator.share) {
+            try {
+              navigator.share({
+                title: 'Custom Message Card',
+                text: 'Click 👉 ',
+                url: shortUrl,
+              });
+            } catch (error) {
+              console.error('Error sharing:', error);
+              prompt('Copy this URL and share it with others:', shortUrl);
+            }
+          } else {
+            prompt('Copy this URL and share it with others:', shortUrl);
+          }
+        } else {
+          alert('Error: ' + data.message);
+        }
+      })
+      .catch(error => {
+        alert('Error: ' + error.message);
+      });
   });
-  const shortUrl = await response.text();
 
-  // show share dialog if supported, otherwise prompt user to copy the link
-  if (navigator.share) {
-    navigator.share({
-      title: 'Custom Message Card',
-      text: 'Click 👉 ',
-      url: shortUrl,
-    });
+  // add click event listener to info button
+  infoButton.addEventListener('click', () => {
+    alert(
+      'Introducing the latest tool by Shashi: the One-Time Message Sender. Iss tool ki madad se aap kisi ko message bhej sakte ho, jo sirf ek baar dikhayi dega, phir hamesha ke liye delete ho jaayega. Yeh tool sensitive information aur private conversations ke liye accha option hai.'
+    );
+  });
+
+  // check if a message, title, and token are present in the URL parameters
+  const urlParams = new URLSearchParams(window.location.search);
+  const message = urlParams.get('message');
+  const title = urlParams.get('title');
+  const token = urlParams.get('token');
+
+  if (message && title && token) {
+    // if a message, title, and token are present, check if the token is valid
+    const viewedToken = localStorage.getItem(token);
+
+    if (!viewedToken) {
+      // if the token is not found in local storage, display the message and title
+      messageElement.innerText = message;
+      titleElement.innerText = title;
+
+      // store the token in local storage to mark it as viewed
+      localStorage.setItem(token, 'viewed');
+    } else {
+      // if the token is found in local storage, show an error message
+      messageElement.innerText = 'This message has already been viewed. Tap to edit and send.';
+      titleElement.innerText = 'Sorry';
+    }
   } else {
-    prompt('Copy this URL and share it with others:', shortUrl);
+    // if no message, title, or token are present, show the default message and title
+    messageElement.innerText = 'Enter your message here';
+    titleElement.innerText = 'Enter Your Name';
   }
 });
-
-// add click event listener to info button
-infoButton.addEventListener('click', () => {
-  alert('Introducing the latest tool by Shashi: the One-Time Message Sender. Iss tool ki madad se aap kisi ko message bhej sakte ho, jo sirf ek baar dikhayi dega, phir hamesha ke liye delete ho jaayega. Yeh tool sensitive information aur private conversations ke liye accha option hai.');
-});
-
-// check if a message, title, and token are present in the URL parameters
-const urlParams = new URLSearchParams(window.location.search);
-const message = urlParams.get('message');
-const title = urlParams.get('title');
-const token = urlParams.get('token');
-
-if (message && title && token) {
-  // if a message, title, and token are present, check if the token is valid
-  const viewedToken = localStorage.getItem(token);
-
-  if (viewedToken === null) {
-    // if the token is valid, show the message and title and store the token in local storage
-    messageElement.innerText = message;
-    titleElement.innerText = title;
-    localStorage.setItem(token, true);
-  } else {
-    // if the token has already been viewed, show the default message and title
-    messageElement.innerText = 'This message has been already viewed. Tap to edit and send.';
-    titleElement.innerText = 'Sorry';
-  }
-} else {
-  // if no message, title, or token are present, show the default message and title
-  messageElement.innerText = 'Enter your message here';
-  titleElement.innerText = 'Enter Your Name';
-}
